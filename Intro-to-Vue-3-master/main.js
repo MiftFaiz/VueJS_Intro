74152a4e5e4c3c8e47f49e4b108977e984ae9f3a
MiftFaiz <siremadu132@gmail.com>
2022-08-29 14:30:19 +0700

    asd

--- Intro-to-Vue-3-master/main.js
+++ Intro-to-Vue-3-master/main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function(){
+        return{
+            product: 'Socks',
+            desc:'This Socks Is Shit',
+            image:'./assets/images/socks_green.jpg'
+        }
+    }
+})
