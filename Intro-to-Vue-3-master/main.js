d634dfd271a620e5b928b38093600d874c1b1ecc
MiftFaiz <59590140+MiftFaiz@users.noreply.github.com>
2022-08-29 15:03:16 +0700

    Delete Intro-to-Vue-3-master directory

--- Intro-to-Vue-3-master/main.js
+++
@@ -1,9 +0,0 @@
-const app = Vue.createApp({
-    data: function(){
-        return{
-            product: 'Socks',
-            desc:'This Socks Is Shit',
-            image:'./assets/images/socks_green.jpg'
-        }
-    }
-})
